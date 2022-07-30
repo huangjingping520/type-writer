48a2f4aba74a49f9741454e62b1a0583a5a1e9c5
MerlinAlex <merlinalex@qq.com>
2022-07-30 21:37:57 +0800

    refactor: monorepo

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { applyPatches, calculatePatch, diff } from '../src/index'
+import { applyPatches, calculatePatch, diff } from '../packages/core/src/index'
 import { input, output } from './fixture'
 
 describe('should', () => {
